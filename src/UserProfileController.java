d739d877df66295bbc873e6084355c677aeb41b3
Aliar Rahman Sajib <68413819+aliarbd@users.noreply.github.com>
2025-01-26 18:28:03 +0600

    -- Added User Profile Page
    Now after succesfully login it will redirect  to the user profile window.

--- src/UserProfileController.java
+++ src/UserProfileController.java
@@ -1,11 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-
+import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -13,6 +23,39 @@ import javafx.fxml.Initializable;
  * @author ur
  */
 public class UserProfileController implements Initializable {
+
+    @FXML
+    private TextField name_edit;
+    @FXML
+    private TextField email_edit;
+    @FXML
+    private TextField phone_edit;
+    @FXML
+    private ComboBox<?> area_edit;
+    @FXML
+    private ComboBox<?> thana_edit;
+    @FXML
+    private ComboBox<?> district_edit;
+    @FXML
+    private ToggleGroup available_radio;
+    @FXML
+    private TextField password_edit;
+    @FXML
+    private Label info_name;
+    @FXML
+    private Label info_email;
+    @FXML
+    private Label info_pass;
+    @FXML
+    private Label info_phone;
+    @FXML
+    private Label info_area;
+    @FXML
+    private Label info_thana;
+    @FXML
+    private Label info_district;
+    @FXML
+    private Label info_available;
 
     /**
      * Initializes the controller class.
